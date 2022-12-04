5853e20985cf748c67042c07e742d175de9fa058
FlorescuMiruna <mirunastefaniaflorescu@gmail.com>
2022-12-04 18:31:43 +0200

    Added style for Register page

--- Frontend/src/app/component/register/register.component.ts
+++ Frontend/src/app/component/register/register.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy{
+  public showLoading: boolean | undefined;
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  public onRegister(user: any): void {
+    this.showLoading = true;
+
+
+      this.showLoading = false;
+    }
+
+    ngOnDestroy(): void {
+    }
 
 }
